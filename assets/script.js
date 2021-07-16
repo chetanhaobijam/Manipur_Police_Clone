dfe6f3ab84686014543cfde5ef559a9cf51f8c5f
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-07-16 17:10:10 +0530

    Make Navbar Responsive

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+console.log("Website script Ready!");
+
+// Menu Toggle Script
+const menu = document.querySelector("#menu-title");
+const navItems = document.querySelector("#nav-items");
+
+menu.addEventListener("click", () => {
+  navItems.classList.toggle("show");
+  menu.classList.toggle("dark-menu")
+  console.log(`Toggle Activated.`);
+})
