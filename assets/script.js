bc2f65928704f8e3a25c1d45246918c317516895
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-07-21 16:19:00 +0530

    Added Proper Lightbox JS

--- assets/script.js
+++ assets/script.js
@@ -10,39 +10,28 @@ menu.addEventListener("click", () => {
 })
 
 // Modal Lighthouse
-// function openModal() {
-//   document.querySelector(".modal").style.display = "block";
-// }
-
-// function closeModal() {
-//   document.querySelector(".modal").style.display = "none";
-// }
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("slides");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   slides[slideIndex-1].style.display = "block";
-// }
 const modal = document.querySelector(".my-modal");
 const openModal = () => {
   modal.style.display = "block";
 }
 const closeModal = () => {
   modal.style.display = "none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+const plusSlides = n => showSlides(slideIndex += n);
+
+const currentSlide = n => showSlides(slideIndex = n);
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
