17737b62ab0ddb72744354b7d1eb9d65d3df3eee
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-07-27 16:14:39 +0530

    Added Font Controls and Contrast Mode JS Functions

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("dark-menu")
 })
 
-// Modal Lighthouse
+// Modal Lightbox
 const modal = document.querySelector(".my-modal");
 const openModal = () => {
   modal.style.display = "block";
@@ -72,3 +72,58 @@ const scrollTop = () => {
 
 document.addEventListener("scroll", handleScroll);
 scrollToTop.addEventListener("click", scrollTop);
+
+// Function to Increase or Decrease Font Size
+const html = document.querySelector("html");
+const decrease = document.querySelector("#font-decrease");
+const original = document.querySelector("#font-original");
+const increase = document.querySelector("#font-increase");
+
+const changeFontSize = (element, factor) => {
+  let style = window.getComputedStyle(element, null).getPropertyValue('font-size');
+  let currentSize = parseFloat(style);
+  element.style.fontSize = (currentSize + factor) + 'px';
+}
+
+// Increase the font-size when clicked
+increase.addEventListener("click", () => {
+  changeFontSize(html, 1);
+})
+
+//Set the default font-size when clicked 
+original.addEventListener("click", () => {
+  html.style.setProperty("font-size", "15px");
+})
+
+// Decrease the font-size when clicked
+decrease.addEventListener("click", () => {
+  changeFontSize(html, -1);
+})
+
+// Function for Changing Contrast Mode (Dark/Light)
+const body = document.querySelector("body");
+const darkMode = document.querySelector("#dark-mode");
+const lightMode = document.querySelector("#light-mode");
+const accessibility = document.querySelector(".accessibility");
+const fontControls = document.querySelector(".font-controls");
+const dgpMessage = document.querySelector(".dgp-message");
+const messageTitle = document.querySelector(".message-title");
+const linkItem = document.querySelectorAll(".link-item");
+
+darkMode.addEventListener("click", () => {
+  body.classList.add("dark-body");
+  accessibility.classList.add("dark-accessibility");
+  fontControls.classList.add("white-text");
+  dgpMessage.classList.add("dark-dgp-message");
+  messageTitle.classList.add("dark-text-shadow");
+  linkItem.forEach(item => item.classList.add("dark-link-item"));
+})
+
+lightMode.addEventListener("click", () => {
+  body.classList.remove("dark-body");
+  accessibility.classList.remove("dark-accessibility");
+  fontControls.classList.remove("white-text");
+  dgpMessage.classList.remove("dark-dgp-message");
+  messageTitle.classList.remove("dark-text-shadow");
+  linkItem.forEach(item => item.classList.remove("dark-link-item"));
+})
